348b0c075308b68b80406bf9f68d0b06edec39f3
viktor-enzell <viktor.enzell@gmail.com>
2021-05-13 17:30:51 +0200

    Load data and init Transformer.

--- transformer.py
+++ transformer.py
@@ -1,6 +1,19 @@
-def load_data():
-    print('hello')
+import torch
+import torch.nn as nn
+from load_data import load_data
+
+
+class TransformerModel(nn.Module):
+    def __init__(self):
+        super(TransformerModel, self).__init__()
+        self.model_type = 'Transformer'
 
 
 if __name__ == '__main__':
-    load_data()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print("Using {} device".format(device))
+
+    batch_size = 50
+
+    train_data, test_data, vocab = load_data(batch_size)
+    model = TransformerModel().to(device)
